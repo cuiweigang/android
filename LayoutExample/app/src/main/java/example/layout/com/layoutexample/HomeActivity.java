232b25d7aea47b7a31628d4273aff9047c2ce898
DESKTOP-BJ41440\cuiweigang <cuiweigang@139.com>
2017-03-20 10:30:21 +0800

    app

--- LayoutExample/app/src/main/java/example/layout/com/layoutexample/HomeActivity.java
+++ LayoutExample/app/src/main/java/example/layout/com/layoutexample/HomeActivity.java
@@ -1,14 +1,19 @@
 package example.layout.com.layoutexample;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import static example.layout.com.layoutexample.R.*;
 
@@ -17,13 +22,15 @@ public class HomeActivity extends AppCompatActivity {
     GridView jgg_GridView;
     ImageView icon_ImageView;
 
-    private Integer[] images = {drawable.main_icon_1, drawable.main_icon_2, drawable.main_icon_3, drawable.main_icon_4, drawable.main_icon_5, drawable.main_icon_6, drawable.main_icon_7, drawable.white_fill, drawable.white_fill};
+    private Integer[] images = {drawable.icon_1, drawable.icon_2, drawable.icon_3, drawable.icon_4, drawable.icon_5, drawable.icon_6, drawable.icon_7, drawable.white_fill, drawable.white_fill};
+    private String[] titles = {"商品管理", "订单管理", "我的收入", "喵客二维码", "佣金计划", "我的喵币", "敬请期待...", "", ""};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(layout.activity_home);
         jgg_GridView = (GridView) findViewById(id.jgg_GridView);
+
         icon_ImageView = (ImageView) findViewById(id.icon_ImageView);
         jgg_GridView.setAdapter(new ImageAdapter(this));
     }
@@ -62,20 +69,26 @@ public class HomeActivity extends AppCompatActivity {
             ItemViewTag viewTag;
             if (convertView == null) {
                 convertView = layoutInflater.inflate(layout.gridview_item, null);
-                viewTag = new ItemViewTag((ImageView) convertView.findViewById(id.icon_ImageView));
+                viewTag = new ItemViewTag((ImageView) convertView.findViewById(id.icon_ImageView), (TextView) convertView.findViewById(id.icon_title));
                 convertView.setTag(viewTag);
             } else {
                 viewTag = (ItemViewTag) convertView.getTag();
             }
             viewTag.image.setImageResource(images[position]);
+            viewTag.text.setText(titles[position]);
+            if (position == 6) {
+                viewTag.text.setTextColor(context.getResources().getColor(color.split_line));
+            }
             return convertView;
         }
 
         class ItemViewTag {
             protected ImageView image;
+            protected TextView text;
 
-            public ItemViewTag(ImageView image) {
+            public ItemViewTag(ImageView image, TextView text) {
                 this.image = image;
+                this.text = text;
             }
         }
     }
